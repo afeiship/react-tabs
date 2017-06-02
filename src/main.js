7e96d65a42e54f7a557ac477aa570253e5376074
afeiship <1290657123@qq.com>
2017-06-02 21:00:29 +0800

    export react-tabs-base

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import ReactTabs from 'components/react-tabs';
+import ReactTabsBase from 'components/react-tabs-base';
 import ReactTabsBody from 'components/react-tabs-body';
 import ReactTabsMenu from 'components/react-tabs-menu';
 export {
   ReactTabs,
+  ReactTabsBase,
   ReactTabsBody,
   ReactTabsMenu
 };
